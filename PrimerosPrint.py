97cd711fdb0e61c06e6b900313c66effc501bd08
ArnoldSb17 <sr.arnold.317@gmail.com>
2022-06-01 20:22:54 -0500

    se le agrega un ?

--- PrimerosPrint.py
+++ PrimerosPrint.py
@@ -2,10 +2,10 @@ nombre = input("INGRESA TU NOMBRE: ")
 edad = int(input("QUE EDAD TIENES: "))
 
 if edad > 119:
-    print("\nHola "+ nombre.capitalize() + ", Que??? tines "+ str(edad) + " años!!!!.\nCreia que la perona con mas edad es la japonesa 'Kane Tanaka' con una edad de 119 años.\n" )
+    print("\nHola "+ nombre.capitalize() + ", Que???? tines "+ str(edad) + " años!!!!.\nCreia que la perona con mas edad es la japonesa 'Kane Tanaka' con una edad de 119 años.\n" )
 
 elif edad >= 18:
-    print("\nHolaa "+ nombre.capitalize() +" Usted cumple la edad minima. \nPuede entrar a ver la pelicula.\n")
+    print("\nHola "+ nombre.capitalize() +" Usted cumple la edad minima. \nPuede entrar a ver la pelicula.\n")
 
 elif edad < 18 and edad > 0:
     estaAcompañado = input("Esta acompañado por un adulto?:").lower()
